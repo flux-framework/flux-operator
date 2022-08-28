b045f59e647cfc1a268e3f36ce6ef1ba521332b8
vsoch <vsoch@users.noreply.github.com>
2022-08-28 13:13:00 -0600

    adding missing LICENSE information to match spack/flux projects
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- controllers/flux/logging.go
+++ controllers/flux/logging.go
@@ -1,5 +1,14 @@
-/* logging has debugging and TBA logging helpers
- */
+/*
+Copyright 2022 Lawrence Livermore National Security, LLC
+ (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+
+This is part of the Flux resource manager framework.
+For details, see https://github.com/flux-framework.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// logging has debugging and TBA logging helpers
 
 package controllers
 
