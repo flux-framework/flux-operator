9eca16a9a397f45bb1e7bb4dd19a3534ee7b42ca
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-09-01 14:15:54 -0600

    add brainstorming docs (#10)
    * add brainstorming docs
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- controllers/flux/fluxsetup_controller.go
+++ controllers/flux/fluxsetup_controller.go
@@ -19,6 +19,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -45,15 +46,7 @@ type FluxSetupReconciler struct {
 //+kubebuilder:rbac:groups=flux-framework.org,resources=pods,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=flux-framework.org,resources=services,verbs=get;list;watch;create;update;patch;delete
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Flux object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.1/pkg/reconcile
+// Reconcile moves the current state of the cluster closer to the desired state.
 func (r *FluxSetupReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
 	// Create a new FluxSetup and Flux instance
