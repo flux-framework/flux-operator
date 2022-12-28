de9dcc14a0b818ec4b40e111ae4cd6cf4f6cb2e5
vsoch <vsoch@users.noreply.github.com>
2022-12-27 18:15:57 -0700

    doh
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- controllers/flux/extra.go
+++ controllers/flux/extra.go
@@ -16,6 +16,7 @@ import (
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
+	networkv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
