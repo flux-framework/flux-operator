d62d36df00ecd877ebe2f32f7373ee7f9f8ad9d4
vsoch <vsoch@users.noreply.github.com>
2023-02-07 17:00:32 -0700

    do not write to /mnt
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- examples/tests/volumes/pre-run.sh
+++ examples/tests/volumes/pre-run.sh
@@ -5,6 +5,6 @@ HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Copying local volume to /tmp/data-volumes in minikube"
 
 # The "data" volume will be mounted at /mnt/data
-minikube ssh -- mkdir -p /mnt/data
-minikube cp ${HERE}/data/pancakes.txt /mnt/data/pancakes.txt
-minikube ssh ls /mnt/data
+minikube ssh -- mkdir -p /tmp/data
+minikube cp ${HERE}/data/pancakes.txt /tmp/data/pancakes.txt
+minikube ssh ls /tmp/data
