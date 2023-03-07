381eba8654336edb39f78c242492da6dac3072f4
vsoch <vsoch@users.noreply.github.com>
2023-03-06 22:01:27 -0700

    try again
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- script/test-python.sh
+++ script/test-python.sh
@@ -10,18 +10,16 @@ cd ${ROOT}
 
 set -eEu -o pipefail
 
-cmd="$@"
-
-echo "Testing Command: ${cmd}"
+echo "Testing Command: $@"
 
 # Quick helper script to run a test
-make clean
-make run &
+make clean > /dev/null 2> /dev/null
+make run > /dev/null 2> /dev/null &
 pid=$!
 echo "PID for running cluster is ${pid}"
 
 # If there is a pre-run script
-${cmd}
+$@
 
 kill ${pid} || true
 kill $(lsof -t -i:8080) || true
