c1bf714f8c2f5884157046c7118fdb5e34b27bdc
vsoch <vsoch@users.noreply.github.com>
2023-03-14 14:15:00 -0600

    more debugging
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- script/test-python.sh
+++ script/test-python.sh
@@ -14,7 +14,7 @@ echo "Testing Command: $@"
 
 # Quick helper script to run a test
 make clean > /dev/null 2> /dev/null
-make run > /dev/null 2> /dev/null &
+make run &
 pid=$!
 echo "PID for running cluster is ${pid}"
 
