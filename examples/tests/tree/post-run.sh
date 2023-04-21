f64ee2903d39d7b57694caf10dfa9cbbae842c71
vsoch <vsoch@users.noreply.github.com>
2023-04-21 00:20:21 -0600

    remove tree from test, make example that is exposed
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- examples/tests/tree/post-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Checking log in minikube"
-minikube ssh -- cat /tmp/data/tree.out
-echo "Cleaning up /tmp/data in minikube"
-minikube ssh -- sudo rm -rf /tmp/data
-
--- examples/tests/tree/pre-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HERE=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-TESTS=$(dirname ${HERE})
-
-# The "data" volume will be mounted at /mnt/data
-minikube ssh -- mkdir -p /tmp/data
