87e9cfc896843eaf4c0414d0bf73532455c8e212
vsoch <vsoch@users.noreply.github.com>
2023-08-18 23:16:56 -0600

    oops forgot command
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ cmd/gen/gen.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/yaml"
+
+	api "github.com/flux-framework/flux-operator/api/v1alpha1"
+	flux "github.com/flux-framework/flux-operator/controllers/flux"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+)
+
+// go run cmd/gen/gen.go -f examples/tests/lammps/minicluster.yaml
+
+var (
+	l         = log.New(os.Stderr, "🟧️fluxoperator: ", log.Ldate|log.Ltime|log.Lshortfile)
+	separator = "----"
+	filename  string
+	includes  string
+	scheme    = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(api.AddToScheme(scheme))
+	utilruntime.Must(corev1.AddToScheme(scheme))
+	utilruntime.Must(batchv1.AddToScheme(scheme))
+}
+
+type IncludePreference struct {
+
+	// By default, we generate all
+	GenVolumes bool
+	GenConfigs bool
+	GenService bool
+	GenJob     bool
+}
+
+// determineIncludes determines what the user wants to print
+func determineIncludes(includes string) IncludePreference {
+
+	// By default, we print all!
+	prefs := IncludePreference{true, true, true, true}
+	if includes == "" {
+		return prefs
+	}
+	// Volumes
+	if !strings.Contains(includes, "v") {
+		prefs.GenVolumes = false
+	}
+	// Config Maps
+	if !strings.Contains(includes, "c") {
+		prefs.GenConfigs = false
+	}
+	// Service
+	if !strings.Contains(includes, "s") {
+		prefs.GenService = false
+	}
+	// Job
+	if !strings.Contains(includes, "j") {
+		prefs.GenJob = false
+	}
+	return prefs
+}
+
+func main() {
+	flag.StringVar(&filename, "f", "", "YAML filename to read it")
+	flag.StringVar(&includes, "i", "", "Custom list of includes (csjv) for cm, svc, job, volume")
+	flag.Parse()
+
+	if filename == "" {
+		l.Fatalf("Please provide a yaml file with -f")
+	}
+
+	// Unless the user has asked
+	decode := serializer.NewCodecFactory(scheme).UniversalDeserializer().Decode
+	stream, err := os.ReadFile(filename)
+	if err != nil {
+		l.Fatalf("Issue reading file %s", err.Error())
+	}
+
+	obj, _, err := decode(stream, nil, nil)
+	if err != nil {
+		l.Fatalf("Issue decoding yaml stream %s", err.Error())
+	}
+
+	// Determine what the person wants to generate
+	prefs := determineIncludes(includes)
+
+	switch cluster := obj.(type) {
+	case *api.MiniCluster:
+
+		// Generate the MiniCluster assets (config maps and indexed job)
+		if prefs.GenConfigs {
+			cms, err := generateMiniClusterConfigs(cluster)
+			if err != nil {
+				l.Fatalf("Issue generating MiniCluster configs %s", err.Error())
+			}
+			for _, cm := range cms {
+				printYaml(cm)
+			}
+		}
+		// Generate the volumes
+		if prefs.GenVolumes {
+			pvs, pvcs := generateMiniClusterVolumes(cluster)
+			for _, pv := range pvs {
+				printYaml(pv)
+			}
+			for _, pvc := range pvcs {
+				printYaml(pvc)
+			}
+		}
+
+		// Service
+		if prefs.GenService {
+
+			svc := generateMiniClusterService(cluster)
+			printYaml(svc)
+		}
+		// Finally the indexed job
+		if prefs.GenJob {
+			job, err := flux.NewMiniClusterJob(cluster)
+			if err != nil {
+				l.Fatalf("Issue generating MiniCluster job %s", err.Error())
+			}
+			if err != nil {
+				l.Fatalf("Issue generating MiniCluster job YAML %s", err.Error())
+			}
+			printYaml(job)
+		}
+	default:
+		l.Fatalf("Type %s is not a MiniCluster", cluster)
+	}
+}
+
+func printYaml(obj runtime.Object) {
+
+	out, err := yaml.Marshal(obj)
+	if err != nil {
+		l.Fatalf("Issue generating MiniCluster job YAML %s", err.Error())
+	}
+	fmt.Println(separator)
+	fmt.Println(string(out))
+}
+
+// generateMiniCluster volumes
+func generateMiniClusterVolumes(cluster *api.MiniCluster) (
+	[]*corev1.PersistentVolume,
+	[]*corev1.PersistentVolumeClaim,
+) {
+
+	pvs := []*corev1.PersistentVolume{}
+	pvcs := []*corev1.PersistentVolumeClaim{}
+
+	// Prepare volumes, if requested, to be available to containers
+	for volumeName, volume := range cluster.Spec.Volumes {
+		pv := flux.CreatePersistentVolume(cluster, volumeName, volume)
+		pvs = append(pvs, pv)
+
+		pvc := flux.CreatePersistentVolumeClaim(cluster, volumeName, volume)
+		pvcs = append(pvcs, pvc)
+	}
+	return pvs, pvcs
+}
+
+// Generate the service for the MiniCluster
+func generateMiniClusterService(cluster *api.MiniCluster) *corev1.Service {
+
+	// Create headless service for the MiniCluster OR single service for the broker
+	selector := map[string]string{"job-name": cluster.Name}
+
+	// If we are adding a minimal service to the index 0 pod only (not supported here)
+	if cluster.Spec.Flux.MinimalService {
+		l.Fatalf("We do not currently support minimal service for this command.")
+	}
+
+	return &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cluster.Spec.Network.HeadlessName,
+			Namespace: cluster.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			ClusterIP: "None",
+			Selector:  selector,
+		},
+	}
+}
+
+// generateMinicluster creates complete Minicluster yaml (Jobset and config maps)
+func generateMiniClusterConfigs(cluster *api.MiniCluster) ([]*corev1.ConfigMap, error) {
+
+	// We will return a set of config maps and a jobset
+	cms := []*corev1.ConfigMap{}
+
+	// Generate entrypoints first
+	data, err := flux.GenerateEntrypoints(cluster)
+	if err != nil {
+		return cms, err
+	}
+	cm := createConfigMap(cluster, cluster.EntrypointConfigMapName(), data)
+	cms = append(cms, cm)
+
+	// flux-config
+	brokerConfig, err := flux.GenerateFluxConfig(cluster)
+	if err != nil {
+		return cms, err
+	}
+	data[flux.HostfileName] = brokerConfig
+	cm = createConfigMap(cluster, cluster.FluxConfigMapName(), data)
+	cms = append(cms, cm)
+
+	// cert generated by zeromq!
+	curveCert, err := flux.GetCurveCert(cluster)
+	if err != nil || curveCert == "" {
+		return cms, err
+	}
+	data[flux.CurveCertKey] = curveCert
+	cm = createConfigMap(cluster, cluster.CurveConfigMapName(), data)
+	cms = append(cms, cm)
+	return cms, nil
+}
+
+// createConfigMap generates a config map with some kind of data
+func createConfigMap(
+	cluster *api.MiniCluster,
+	configName string,
+	data map[string]string,
+) *corev1.ConfigMap {
+
+	// Create the config map with respective data!
+	// Likely we shouldn't hard code this :)
+	cm := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      configName,
+			Namespace: cluster.Namespace,
+		},
+		Data: data,
+	}
+	return cm
+}
