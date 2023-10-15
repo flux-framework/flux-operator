7b3dde6a238ff464cec9b21eeae5aab24ea510f5
vsoch <vsoch@users.noreply.github.com>
2023-10-15 10:14:49 -0600

    fix namespace
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- examples/tests/existing-volumes/post-run.sh
+++ examples/tests/existing-volumes/post-run.sh
@@ -6,7 +6,7 @@ echo "Cleaning up /tmp/data in minikube"
 
 # pods usually need to be deleted before the pvc/pv
 kubectl delete -f ${HERE}/minicluster.yaml
-kubectl delete -n flux-operator pods --all --grace-period=0 --force
-kubectl delete -n flux-operator pvc --all --grace-period=0 --force
-kubectl delete -n flux-operator pv --all --grace-period=0 --force
+kubectl delete pods --all --grace-period=0 --force
+kubectl delete pvc --all --grace-period=0 --force
+kubectl delete pv --all --grace-period=0 --force
 minikube ssh -- sudo rm -rf /tmp/data
