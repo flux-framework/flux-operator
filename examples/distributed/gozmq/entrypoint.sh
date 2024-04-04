19785be42a388c1677bf0ef09a1b632dc0690d97
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2024-04-04 16:11:47 -0600

    update gozmq example (#225)
    * update gozmq example
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- examples/distributed/gozmq/entrypoint.sh
+++ examples/distributed/gozmq/entrypoint.sh
@@ -6,4 +6,4 @@ rank=${FLUX_TASK_RANK}
 # Get the host name
 host=$(hostname)
 echo "Hello I'm host $host"
-go run /code/main.go run --size ${workers} --prefix flux-sample --suffix "flux-service.default.svc.cluster.local" --port 5555 --rank ${rank}
+go run /code/main.go run --size ${workers} --prefix flux-sample --suffix "flux-service.default.svc.cluster.local" --port 5555 --rank ${rank} --raw
