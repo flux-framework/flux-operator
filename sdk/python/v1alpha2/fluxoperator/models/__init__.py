8a084db7167c3e7b38834b49dc95945534be0b10
vsoch <vsoch@users.noreply.github.com>
2025-07-20 14:49:42 -0700

    bump: go version to 1.23
    Also add an identifying app label for the flux operator
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- sdk/python/v1alpha2/fluxoperator/models/__init__.py
+++ sdk/python/v1alpha2/fluxoperator/models/__init__.py
@@ -14,25 +14,3 @@
 
 
 # import models into model package
-from fluxoperator.models.bursted_cluster import BurstedCluster
-from fluxoperator.models.bursting import Bursting
-from fluxoperator.models.commands import Commands
-from fluxoperator.models.container_resources import ContainerResources
-from fluxoperator.models.container_volume import ContainerVolume
-from fluxoperator.models.flux_broker import FluxBroker
-from fluxoperator.models.flux_container import FluxContainer
-from fluxoperator.models.flux_scheduler import FluxScheduler
-from fluxoperator.models.flux_spec import FluxSpec
-from fluxoperator.models.life_cycle import LifeCycle
-from fluxoperator.models.logging_spec import LoggingSpec
-from fluxoperator.models.mini_cluster import MiniCluster
-from fluxoperator.models.mini_cluster_archive import MiniClusterArchive
-from fluxoperator.models.mini_cluster_container import MiniClusterContainer
-from fluxoperator.models.mini_cluster_list import MiniClusterList
-from fluxoperator.models.mini_cluster_spec import MiniClusterSpec
-from fluxoperator.models.mini_cluster_status import MiniClusterStatus
-from fluxoperator.models.mini_cluster_user import MiniClusterUser
-from fluxoperator.models.network import Network
-from fluxoperator.models.pod_spec import PodSpec
-from fluxoperator.models.secret import Secret
-from fluxoperator.models.security_context import SecurityContext
